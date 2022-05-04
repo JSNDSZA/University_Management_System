092a11718153349eec2b4b7e76d163587ebfaa75
Jason Dsouza <jasondsouza523@gmail.com>
2022-05-04 11:33:51 +0530

    Update Student.java

--- student-management-system/src/main/java/com/jason/sms/entity/Student.java
+++ student-management-system/src/main/java/com/jason/sms/entity/Student.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity 
+@Entity                                                      //mapped to data table
 @Table(name = "students")
 public class Student {
     
@@ -15,7 +15,7 @@ public class Student {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@Column(name = "first_name", nullable = false)
+	@Column(name = "first_name", nullable = false)                         //Table columns
      private String firstName;
 	
 	@Column(name = "last_name")
